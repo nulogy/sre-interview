389844879cb2aeca211c2475b338581b77b7ff94
Rafael Moraes <rafops@users.noreply.github.com>
2025-04-30 14:17:55 -0400

    Adds pgclient

---
+++ entrypoint.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+until pg_isready -h "$PGHOST" -p "5432" -U "postgres"; do
+  echo "Waiting for PostgreSQL..."
+  sleep 2
+done
+
+psql "host=$PGHOST port=5432 user=postgres dbname=postgres" <<EOF
+CREATE TABLE IF NOT EXISTS random_numbers (
+    id SERIAL PRIMARY KEY,
+    value SMALLINT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+EOF
+
+while true; do
+  psql "host=$PGHOST port=5432 user=postgres dbname=postgres" <<EOF
+INSERT INTO random_numbers (value) VALUES ($RANDOM);
+EOF
+  echo "Added $VALUE"
+  sleep 1
+done
